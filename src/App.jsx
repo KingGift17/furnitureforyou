9b7d5c0f931afd1d3c16e97e9dbc212cea331c83
KingGift17 <obazunu17@gmail.com>
2023-07-27 12:19:23 +0100

    Added all the pages + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Homepage from "./pages/Homepage";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ProductDetails from "./pages/ProductDetails";
+import Shop from "./pages/Shop";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/productdetails" element={<ProductDetails />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
