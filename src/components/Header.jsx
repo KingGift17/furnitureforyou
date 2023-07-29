0e2978a402f38127c54878e6211e9475fa5017a6
KingGift17 <obazunu17@gmail.com>
2023-07-29 11:54:06 +0100

    started header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 import "../styles/components/header.css";
+import logo from "../assets/images/eco-logo.png";
 
-import { Container } from "reactstrap";
+import { Container, Row } from "reactstrap";
+import { Link } from "react-router-dom";
 
-function Header() {
-  return <div>Header</div>;
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav-wrapper">
+            <div className="logo">
+              <img src={logo} alt="" />
+              <div>
+                <h1>GiftKart</h1>
+                <p>Been in Supply and Demand since 2023</p>
+              </div>
+            </div>
+
+            <div className="navbar">
+              <ul className="menu">
+                <li className="nav_item">
+                  <Link to="/home">Home</Link>
+                </li>
+                <li className="nav_item">
+                  <Link to="/shop">Shop</Link>
+                </li>
+                <li className="nav_item">
+                  <Link to="/cart">Cart</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="nav_icons">
+              <span className="cart_icon"></span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
 export default Header;
