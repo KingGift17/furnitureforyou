212ba9e7ac8d457ea9b46d57c9854ebbce3a557c
KingGift17 <obazunu17@gmail.com>
2023-07-29 11:22:05 +0100

    Added header and container installed reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Cart from "./pages/Cart";
@@ -7,20 +12,27 @@ import ProductDetails from "./pages/ProductDetails";
 import Shop from "./pages/Shop";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/shop/:id" element={<ProductDetails />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/shop/:id" element={<ProductDetails />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import "../styles/components/header.css";
+
+import { Container } from "reactstrap";
 
 function Header() {
   return <div>Header</div>;
