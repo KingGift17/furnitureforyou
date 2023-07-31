110f238db917f4c179d32370770dbbdd83202781
KingGift17 <obazunu17@gmail.com>
2023-07-31 09:15:16 +0100

    Finished template for productCards and link shopid

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,6 +3,8 @@ import productImg from "../assets/images/arm-chair-01.jpg";
 import AddIcon from "@mui/icons-material/Add";
 import "../styles/components/ProductCard.scss";
 import { Col } from "reactstrap";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
@@ -12,14 +14,18 @@ const ProductCard = () => {
           <img src={productImg} alt="" />
         </div>
         <div className="p-2 product-info">
-          <h3 className="product-name">Modern Armchair</h3>
-          <span className="text-center d-block">Chair</span>
+          <h3 className="product-name">
+            <Link to="/shop/id">Modern Armchair</Link>
+          </h3>
+          <span>Chair</span>
         </div>
         <div className="product-card-bottom d-flex align-items-center justify-content-between p-2">
           <span className="price">£299</span>
-          <span>
-            <AddIcon />
-          </span>
+          <motion.span whileTap={{ scale: 1.2 }}>
+            <i>
+              <AddIcon />
+            </i>
+          </motion.span>
         </div>
       </div>
     </Col>
