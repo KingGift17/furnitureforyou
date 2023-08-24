3265b546a125c04a5e91ec7552522ceed4032542
KingGift17 <obazunu17@gmail.com>
2023-08-24 23:08:44 +0100

    adding scss to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,19 @@ import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <Container>
         <Row>
           <Col lg="4">
             <div className="logo">
-              <img src={logo} />
               <div>
-                <h1>GiftKart</h1>
+                <h1 className="text-white">GiftKart</h1>
               </div>
             </div>
-            <p className="footer-text mt-4">
+            <p className="footer-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
               dolorem consequuntur eum quam qui delectus quae sed, consequatur
               natus quos.
@@ -106,6 +107,13 @@ function Footer() {
                 </ListGroupItem>
               </ListGroup>
             </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer_copyright">
+              Copyright &copy;{year} developed by Ovie G Bazunu. All rights
+              reserved.
+            </p>
           </Col>
         </Row>
       </Container>
