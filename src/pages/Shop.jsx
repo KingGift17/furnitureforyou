fbabd174f3465ad0477d4a49ba34ae7f66062bc9
KingGift17 <obazunu17@gmail.com>
2023-09-13 08:26:44 +0100

    filter for the shop page is added

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet";
 import { Col, Container, Row } from "reactstrap";
 import SearchIcon from "@mui/icons-material/Search";
 import "../styles/pages/Shop.scss";
+import products from "../assets/data/product";
+import ProductsList from "../components/UI/ProductsList";
 
 const Shop = () => {
+  const [productsData, setProductsData] = useState(products);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === "mobile") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "mobile"
+      );
+      setProductsData(filteredProducts);
+    } else if (filterValue === "watch") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "watch"
+      );
+      setProductsData(filteredProducts);
+    } else if (filterValue === "sofa") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "sofa"
+      );
+      setProductsData(filteredProducts);
+    } else if (filterValue === "chair") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "chair"
+      );
+      setProductsData(filteredProducts);
+    } else if (filterValue === "wireless") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "wireless"
+      );
+      setProductsData(filteredProducts);
+    }
+  };
+
   return (
     <Helmet title="Shop">
       <CommonSection title="Products" />
@@ -15,7 +49,7 @@ const Shop = () => {
           <Row>
             <Col lg="3" md="3">
               <div className="filter-widget">
-                <select>
+                <select onChange={handleFilter}>
                   <option>Filter By Category</option>
                   <option value="mobile">Mobile</option>
                   <option value="watch">Watch</option>
@@ -42,6 +76,17 @@ const Shop = () => {
                 <input type="text" placeholder="Search" />
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            {productsData.length === 0 ? (
+              <h1>No products are found</h1>
+            ) : (
+              <ProductsList data={productsData} />
+            )}
           </Row>
         </Container>
       </section>
