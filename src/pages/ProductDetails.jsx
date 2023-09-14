7621edd11fa0448b577432271a8fe88a46575b23
ob97 <ob97@student.le.ac.uk>
2023-09-14 15:39:43 +0100

    started adding in product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,16 +3,53 @@ import products from "../assets/data/product";
 import Helmet from "../components/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import { Col, Container, Row } from "reactstrap";
+import { useParams } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
 
 const ProductDetails = () => {
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+
+  const {
+    imgUrl,
+    productName,
+    price,
+    avgRating,
+    review,
+    description,
+    shortDesc,
+  } = product;
+
   return (
     <Helmet>
       <CommonSection />
-      <section>
+      <section className="pt-0">
         <Container>
           <Row>
-            <Col lg="6"></Col>
-            <Col lg="6"></Col>
+            <Col lg="6">
+              <img src={imgUrl} alt="" />
+            </Col>
+            <Col lg="6">
+              <div className="product-details">
+                <h2>{productName}</h2>
+                <div className="product-rating">
+                  <div>
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarHalfIcon />
+                  </div>
+                  <p>{avgRating} Rating</p>
+                </div>
+
+                <span>£{price}</span>
+                <p>{shortDesc}</p>
+
+                <button className="buy-btn">Add to Cart</button>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
