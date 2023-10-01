ecd1d708ff5dfe584ae58199bae2b174186de755
ob97 <ob97@student.le.ac.uk>
2023-10-01 22:21:48 +0100

    Cart Page completed with subtotal and buttons

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,9 +7,11 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { motion } from "framer-motion";
 import { cartActions } from "../redux/slices/CartSlices";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
   return (
     <Helmet title="Cart">
@@ -39,7 +41,23 @@ const Cart = () => {
                 </table>
               )}
             </Col>
-            <Col lg="3"></Col>
+            <Col lg="3">
+              <div>
+                <h6 className="d-flex align-items-center justify-content-between">
+                  Subtotal
+                </h6>
+                <span className="fs-4 fw-bold">£{totalAmount}</span>
+              </div>
+              <p className="fs-6 mt-2">Taxes and Vax exc.</p>
+              <div>
+                <button className="checkout-btn">
+                  <Link to="/checkout">Checkout</Link>
+                </button>
+                <button className="checkout-btn">
+                  <Link to="/shop">Continue Shopping</Link>
+                </button>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
