7fdb4bd8d10bcc30b29f7c5a3aa6230790f61a24
ob97 <ob97@student.le.ac.uk>
2023-10-02 09:38:07 +0100

    started login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Helmet from "../components/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/pages/Login.scss";
 
-function Login() {
-  return <div>Login</div>;
-}
+const Login = () => {
+  return (
+    <Helmet title="Login">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold fs-4">Login</h3>
+
+              <Form className="auth-form">
+                <FormGroup className="form-group">
+                  <input type="email" placeholder="Enter your email"></input>
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                  ></input>
+                </FormGroup>
+                <button className="checkout-btn auth-btn">Login</button>
+                <p>
+                  Dont have an account?{" "}
+                  <Link to="/register">Register here</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
 
 export default Login;
