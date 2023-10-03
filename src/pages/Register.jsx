4ae3e8e4c0dfdb72977117a3b5f54759e8499e9d
ob97 <ob97@student.le.ac.uk>
2023-10-03 10:16:31 +0100

    added some firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Helmet from "../components/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.config";
+
 import "../styles/pages/Login.scss";
 
 const Register = () => {
@@ -9,6 +12,22 @@ const Register = () => {
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
   const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const register = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error) {}
+  };
 
   return (
     <Helmet title="Register">
@@ -18,7 +37,7 @@ const Register = () => {
             <Col lg="6" className="m-auto text-center">
               <h3 className="fw-bold mb-4">Register</h3>
 
-              <Form className="auth-form">
+              <Form className="auth-form" onSubmit={register}>
                 <FormGroup className="form-group">
                   <input
                     type="text"
